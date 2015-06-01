18523423d0b144060dc481a55ed6e26ca654bf81
Izaak Schroeder <izaak.schroeder@gmail.com>
2015-06-01 02:29:27 -0700

    No errors anymore at least...

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Load SVG icons.
 
+TODO:
+ * [ ] Allow individual icon loading
+
 Configure webpack. To use icon fonts, [font-loader] must also be enabled.
 
 ```javascript
@@ -48,3 +51,30 @@ export default class Something extends Component {
 	}
 }
 ```
+
+Using multiple icon libraries:
+
+```javascript
+// Import all the icons you want to use.
+import FontAwesome from 'icons-fontawesome';
+import MyIcons from './icons-sample';
+
+// It can be useful to give semantic meaning to your icons, encoding their
+// purpose instead of just their glyph.
+const Aliases = {
+	Error: FontAwesome.Alert,
+	Success: MyIcons.Checkmark
+};
+
+// Export everything.
+export {...FontAwesome ...MyIcons ...Aliases};
+```
+
+TODO:
+ * Support for ligatures https://github.com/nfroidure/svgicons2svgfont/pull/27
+ * Support spritemaps for non-svg
+ * Support automatic retina sizing for non-svg
+ * Support for inline svg via https://github.com/iconic/SVGInjector
+
+https://github.com/jakesgordon/bin-packing
+https://github.com/richardbutler/node-spritesheet
---
+++ example/webpack.config.js
@@ -0,0 +1,54 @@
+
+var path = require('path'),
+	webpack = require('webpack'),
+	ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// Export the webpack configuration
+module.exports = {
+	entry: {
+		'test.css': './test.css',
+		// 'test.scss.css': './test.scss'
+		// 'test.js': './test.js'
+	},
+
+	// Output controls the settings for file generation.
+	output: {
+		filename: '[name].[hash].js',
+		path: path.join(__dirname, 'build'),
+		chunkFilename: '[id].[hash].js'
+	},
+
+	// Module settings.
+	module: {
+		loaders: [{
+			test: /\.css$/,
+			loaders: [
+				ExtractTextPlugin.loader({
+					extract: true,
+					omit: 1
+				}),
+				'style',
+				'css?importLoaders=2',
+				'icon',
+				'font?format[]=truetype&format[]=woff&format[]=embedded-opentype'
+			]
+		}, {
+			test: /\.scss$/,
+			loaders: [
+				ExtractTextPlugin.loader({
+					extract: true,
+					omit: 1
+				}),
+				'style',
+				'css?importLoaders=2',
+				'icon',
+				'font?format[]=truetype&format[]=woff&format[]=embedded-opentype',
+				'sass'
+			]
+		}]
+	},
+
+	plugins: [
+		new ExtractTextPlugin('[name].[hash].css')
+	]
+};
